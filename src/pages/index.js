676dcc6a993c7ede76513c9380b6a34467eee356
hungrysquirrel <joshmaxrubinstein@gmail.com>
2021-12-29 19:34:24 -0800

    Lower res image

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => {
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Thomas the dog"
-        src="../images/thomas.jpeg"
+        src="../images/thomas.jpg"
       />
     </Layout>
   )
