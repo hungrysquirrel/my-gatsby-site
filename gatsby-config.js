13ad92a72eb1cdc8a64de58ceecbfdec60ee4a5c
hungrysquirrel <joshmaxrubinstein@gmail.com>
2021-12-30 09:31:57 -0800

    Added a blog section
    - read from data source
    - print blog page names

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "My Gatsby Site",
+    title: "My Gatsby Site will rock the internet in 2022",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import { StaticImage } from 'gatsby-plugin-image'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial. Yeehaa!</p>
+      <p>I'm making this by following the Gatsby Tutorial. Yeehaa! 2022</p>
       <StaticImage
         alt="Thomas the dog"
         src="../images/thomas.jpg"
