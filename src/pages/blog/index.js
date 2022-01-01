9142702baa2e9d4e859366f88c65c52c5cae3d47
hungrysquirrel <joshmaxrubinstein@gmail.com>
2022-01-01 09:05:19 -0800

    generate dynamic blog pages

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,6 @@
 import * as React from 'react'
-import Layout from '../components/layout'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
+import Layout from '../../components/layout'
+import { Link, graphql } from 'gatsby'
 
 
 const BlogPage = ({data}) => {
@@ -10,11 +9,8 @@ const BlogPage = ({data}) => {
       {
         data.allMdx.nodes.map((node) => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <Link to={`/blog/${node.slug}`}><h2>{node.frontmatter.title}</h2></Link>
             <p>Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>
-              {node.body}
-            </MDXRenderer>
           </article>
         ))
       }
@@ -31,7 +27,7 @@ export const query = graphql`
           title
         }
         id
-        body
+        slug
       }
     }
   }
